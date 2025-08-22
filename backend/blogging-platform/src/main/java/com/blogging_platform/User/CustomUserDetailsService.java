4d3fa0967b8e90d7b38f6305bc0b456a7dc36bc4
saisrinivas000 <saisrinivas.b.offical@gmail.com>
2025-08-22 14:55:16 +0530

    implemented custom user details service..!

---
+++ backend/blogging-platform/src/main/java/com/blogging_platform/User/CustomUserDetailsService.java
@@ -0,0 +1,31 @@
+package com.blogging_platform.User;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.blogging_platform.exceptions.UserNotFoundException;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService{
+	
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userRepository.findUserByUsername(username)
+									.orElseThrow(
+											()-> new UserNotFoundException("User not found..!")
+											);
+		
+		return org.springframework.security.core.userdetails.User
+															.withUsername(user.getUsername())
+															.password(user.getPassword())
+															.roles(user.getRole().name())
+															.build();
+	}
+
+}
